92a639909f6da3908096b70927d1bf69937d2d24
PS8924 <shah.pankti@northeastern.edu>
2024-07-27 14:35:32 -0700

    updates in languages section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
 import Summary from "./components/Summary/Summary";
-import Language from "./components/Languages/Language";
+import Language from "./components/Languages/Languages";
 
 const App = () =>{
   return (
